9d6865e0351110145d1468b2140f104e69a273a2
Giorgio Perez <giorgiop62@gmail.com>
2023-03-20 16:27:58 +0100

    first

--- index.php
+++ index.php
@@ -5,11 +5,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/styles">
     <title>Edusogno</title>
 </head>
 
-<body>
+<h2>Sei già registrato?</h2>
 
+<body>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  <div class="slidecontainer">
+    <input type="range" name="lunghezzaPass" min="1" max="64" value="0">
+  </div>
+<input type="submit" name="submit" value="Genera">
+</form>
+<div><?php echo $password ?></div>
+
+    
 </body>
 
 </html>
